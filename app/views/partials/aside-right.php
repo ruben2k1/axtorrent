f3c28a6824cb261c047d5f79d49613e42503913b
Rubén <56169753+ruben2k1@users.noreply.github.com>
2023-10-25 20:00:16 +0200

    Add files

---
+++ app/views/partials/aside-right.php
@@ -0,0 +1,11 @@
+<aside class="aside-right">
+    <h1>BUSCAR PELÍCULA POR FORMATO</h1>
+    <ul>
+        <li><a href="">4K</a></li>
+        <li><a href="">BDREMUX 1080P</a></li>
+        <li><a href="">BLUE-RAY 1080P</a></li>
+        <li><a href="">BLUE-RAY 720P</a></li>
+        <li><a href="">MICROHD 1080P</a></li>
+        <li><a href="">MICROHD 720P</a></li>
+    </ul>
+</aside>
