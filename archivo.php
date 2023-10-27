8fe95bcaf6d6d15441474befd8be7f5bbd6c94d8
Rubén <56169753+ruben2k1@users.noreply.github.com>
2023-10-27 03:58:12 +0200

    Update files

--- archivo.php
+++ archivo.php
@@ -1,52 +1,65 @@
-<?php
-    require('app/database/db.php');
+<?php require('app/controllers/archivoController.php'); ?>
 
-    $id = $_GET['id'];
-
-    $db = new Connection('localhost', 'filedown', 3306, 'root', '');
-
-    $sentence1 = $db->prepare("SELECT * FROM ARCHIVOS WHERE ID = ?");
-    $sentence1->bindParam(1, $id);
-    $sentence1->execute();
-    $results1 = $sentence1->fetch();
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> <?php echo $results1['TITULO'] ?> - AXTorrent</title>
+    <title>Archivos - AXTorrent</title>
+    <link rel="stylesheet" href="public/css/reset.css">
     <link rel="stylesheet" href="public/css/index.css">
     <link rel="shortcut icon" href="public/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 </head>
 </head>
 <body>
-    <?php
-        require('app/views/partials/header-logout.php');
-    ?>
+    <?php require('app/views/partials/header.php'); ?>
 
     <div class="container">
-        <?php
-            require('app/views/partials/aside.php');
-        ?>
+        <?php require('app/views/partials/aside-left.php'); ?>
 
         <main>
-            <h2>📂 <?php echo $results1['TITULO'] ?></h2>
-            
-            <?php
-                echo "
+            <div class="main-h2">
+                <h2>ARCHIVO</h2>
+            </div>
+            <div class="main-article">
+                <article>
+                    <img src="<?php echo $results1['EXT_IMG_ROUTE'] ?>" alt="">
+                </article>
                 <article>
                     <ul>
-                        <li><img src='{$results1['RUTA_IMG']}' alt=''></li>
-                        <li>{$results1['TITULO']}</li>
-                        <button><a href='{$results1['RUTA_ARCHIVO']}' download>DESCARGAR</a></button>
+                        <li>FORMATO: <?php echo $results1['FORMAT'] ?></li>
+                        <li>EPISODIOS: <?php echo $results2['COUNT'] ?></li>
+                        <li>DESCRIPCIÓN: <?php echo $results1['DESCRIPTION'] ?></li>
                     </ul>
+                    <table>
+                        <thead>
+                          <tr>
+                            <th>EPISODIOS</th>
+                            <th>ENLACE</th>
+                            <th>FECHA</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          
+                            <?php
+                                foreach ($results3 as $result) {
+                                    echo "
+                                    <tr>
+                                        <td>{$result['EPISODE']}</td>
+                                        <td><a href='{$result['EXT_FILE_ROUTE']}' download>DESCARGAR</a></td>
+                                        <td>{$result['DATE']}</td>
+                                    </tr>";
+                                }
+                            ?>
+                        </tbody>
+                    </table>
                 </article>
-                ";
-            ?>
+            </div>
         </main>
+
+        <?php require('app/views/partials/aside-right.php'); ?>
     </div>
 </body>
 </html>
