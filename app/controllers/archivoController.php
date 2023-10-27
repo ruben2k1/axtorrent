cfa1542faaea9f851212e27461dc72e2542db772
Rubén <56169753+ruben2k1@users.noreply.github.com>
2023-10-27 03:58:07 +0200

    Add files

---
+++ app/controllers/archivoController.php
@@ -0,0 +1,32 @@
+<?php
+    require('app/database/db.php');
+
+    $db = new Connection('localhost', 'axtorrent', 3307, 'root', 'EC5B09B113AC14D6FF0481665B469AA560CE662E7E87BF57C344FC4E03844B8C');
+
+    if (!isset($_GET['id']) || empty($_GET['id'])) {
+        header('Location: index.php');
+        die();
+    }
+
+    $id = $_GET['id'];
+
+    $sentence1 = $db->prepare("SELECT * FROM FILES WHERE ID = ?");
+    $sentence1->bindParam(1, $id);
+    $sentence1->execute();
+    $results1 = $sentence1->fetch();
+
+    if ($results1 === false) {
+        header('Location: index.php');
+        die();
+    }
+
+    $sentence2 = $db->prepare("SELECT COUNT(*) AS COUNT FROM EPISODES WHERE FILE_ID = ?");
+    $sentence2->bindParam(1, $id);
+    $sentence2->execute();
+    $results2 = $sentence2->fetch();
+
+    $sentence3 = $db->prepare("SELECT * FROM EPISODES WHERE FILE_ID = ?");
+    $sentence3->bindParam(1, $id);
+    $sentence3->execute();
+    $results3 = $sentence3->fetchAll();
+?>
