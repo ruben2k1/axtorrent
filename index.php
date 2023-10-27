270263936812ac104d69375bfc85de8e8e58a4eb
Rubén <56169753+ruben2k1@users.noreply.github.com>
2023-10-27 23:40:28 +0200

    Update files

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio - AXTorrent</title>
@@ -11,7 +10,14 @@
     <link rel="stylesheet" href="public/css/index.css">
     <link rel="shortcut icon" href="public/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-</head>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-K9PYVMMV1W"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-K9PYVMMV1W');
+    </script>
 </head>
 <body>
     <?php require('app/views/partials/header.php'); ?>
