b169a5b7e448b3fe1a4f36e69fc634dc013597e4
Rubén <56169753+ruben2k1@users.noreply.github.com>
2023-10-28 02:04:38 +0200

    Update files

--- index.php
+++ index.php
@@ -5,11 +5,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inicio - AXTorrent</title>
+    <title>AXTorrent - Descargar Películas y Series Gratis en Español</title>
+    <meta name="description" content="AXTorrent - Descargar y Ver Estrenos de Series y Pelis Torrent Gratis en Español. 4K, HD, VOSE y gran variedad de géneros sin publicidad">
+    <meta property="og:locale" content="es_ES">
+    <meta property="og:url" content="https://axtorrent.com">
+    <meta property="og:site_name" content="AXTorrent - Descargar Películas y Series Torrent Gratis">
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:title" content="AXTorrent - Descargar Películas y Series Gratis en Español">
+    <meta name="twitter:description" content="AXTorrent - Descargar y Ver Estrenos de Series y Pelis Torrent Gratis en Español. 4K, HD, VOSE y gran variedad de géneros sin publicidad">
+    <meta name="twitter:image" content="public/logo.svg">
     <link rel="stylesheet" href="public/css/reset.css">
     <link rel="stylesheet" href="public/css/index.css">
     <link rel="shortcut icon" href="public/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <?php require('app/views/partials/cmp.php'); ?>
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-K9PYVMMV1W"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
