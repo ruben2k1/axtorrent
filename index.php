bb98021048e259b1d7ffe54489da4f111bb65937
Rubén <56169753+ruben2k1@users.noreply.github.com>
2023-10-30 16:40:26 +0100

    Update files

--- index.php
+++ index.php
@@ -18,8 +18,6 @@
     <link rel="stylesheet" href="public/css/index.css">
     <link rel="shortcut icon" href="public/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-    <?php require('app/views/partials/cmp.php'); ?>
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-K9PYVMMV1W"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
