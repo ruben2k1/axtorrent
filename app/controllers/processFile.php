c4584c55d315b02285603bb8f5ce416eeefffcf4
Rubén <56169753+ruben2k1@users.noreply.github.com>
2023-11-09 01:25:44 +0100

    Update files

--- app/controllers/processFile.php
+++ app/controllers/processFile.php
@@ -14,144 +14,186 @@
     $ext_img_route = $_POST['ext_img_route'];
     $userid = 1;
 
-    if (empty($genre) && empty($director) && empty($cast)) {
-        //Subir SERIE, DOCUMENTAL
-        
-        $sentence1 = $db->prepare("INSERT INTO files (TITLE, DESCRIPTION, TYPE, FORMAT, EXT_IMG_ROUTE, USER_ID) VALUES (?, ?, ?, ?, ?, ?)");
-        $sentence1->bindParam(1, $title);
-        $sentence1->bindParam(2, $description);
-        $sentence1->bindParam(3, $type);
-        $sentence1->bindParam(4, $format);
-        $sentence1->bindParam(5, $ext_img_route);
-        $sentence1->bindParam(6, $userid);
-        $sentence1->execute();
+    switch ($type) {
+        case 'SERIE':
+            $sentence1 = $db->prepare("INSERT INTO files (TITLE, DESCRIPTION, TYPE, FORMAT, EXT_IMG_ROUTE, USER_ID) VALUES (?, ?, ?, ?, ?, ?)");
+            $sentence1->bindParam(1, $title);
+            $sentence1->bindParam(2, $description);
+            $sentence1->bindParam(3, $type);
+            $sentence1->bindParam(4, $format);
+            $sentence1->bindParam(5, $ext_img_route);
+            $sentence1->bindParam(6, $userid);
+            $sentence1->execute();
 
-        $file_id = intval($db->lastInsertId());
+            $file_id = intval($db->lastInsertId());
 
-        $episode_titles = $_POST['episode_title'];
-        $episode_int_file_routes = $_FILES['episode_int_file_route'];
-        $episode_dates = $_POST['episode_date'];
+            $episode_titles = $_POST['episode_title'];
+            $episode_int_file_routes = $_FILES['episode_int_file_route'];
+            $episode_dates = $_POST['episode_date'];
+            
+            if(isset($_FILES['episode_int_file_route'])) {
+                $numArchivos = count($_FILES['episode_int_file_route']['name']);
+            
+                for($i = 0; $i < $numArchivos; $i++) {
+                    $lastEpisodeId = getNextIdEpisodes($db);
+                    $int_file = 'public/files/' . $lastEpisodeId . '.torrent';
+                                    
+                    $nombreArchivo = $_FILES['episode_int_file_route']['name'][$i];
+                    $tipoArchivo = $_FILES['episode_int_file_route']['type'][$i];
+                    $tmpNombre = $_FILES['episode_int_file_route']['tmp_name'][$i];
         
-        if(isset($_FILES['episode_int_file_route'])) {
-            $numArchivos = count($_FILES['episode_int_file_route']['name']);
+                    $sentence2 = $db->prepare("INSERT INTO episodes (ID, EPISODE, INT_FILE_ROUTE, DATE, FILE_ID) VALUES (?, ?, ?, ?, ?)");
+                    $sentence2->bindParam(1, $lastEpisodeId);
+                    $sentence2->bindParam(2, $episode_titles[$i]);
+                    $sentence2->bindParam(3, $int_file);
+                    $sentence2->bindParam(4, $episode_dates[$i]);
+                    $sentence2->bindParam(5, $file_id);
+                    $sentence2->execute();
         
-            for($i = 0; $i < $numArchivos; $i++) {
-                $lastEpisodeId = getNextIdEpisodes($db);
-                $int_file = 'public/files/' . $lastEpisodeId . '.torrent';
-                                
-                $nombreArchivo = $_FILES['episode_int_file_route']['name'][$i];
-                $tipoArchivo = $_FILES['episode_int_file_route']['type'][$i];
-                $tmpNombre = $_FILES['episode_int_file_route']['tmp_name'][$i];
-    
-                $sentence2 = $db->prepare("INSERT INTO episodes (ID, EPISODE, INT_FILE_ROUTE, DATE, FILE_ID) VALUES (?, ?, ?, ?, ?)");
-                $sentence2->bindParam(1, $lastEpisodeId);
-                $sentence2->bindParam(2, $episode_titles[$i]);
-                $sentence2->bindParam(3, $int_file);
-                $sentence2->bindParam(4, $episode_dates[$i]);
-                $sentence2->bindParam(5, $file_id);
-                $sentence2->execute();
-    
-                $rutaDestino = "../../public/files/" . $lastEpisodeId . '.torrent';
-                move_uploaded_file($tmpNombre, $rutaDestino);
-                
-                echo "El archivo $nombreArchivo se subió correctamente.";
+                    $rutaDestino = "../../public/files/" . $lastEpisodeId . '.torrent';
+                    move_uploaded_file($tmpNombre, $rutaDestino);
+                    
+                    echo "El archivo $nombreArchivo se subió correctamente.";
+                }
             }
-        }
-
-        header('Location: ../../dashboard.php');
-        die();
-    } elseif (empty($description) && empty($director) && empty($format) && empty($cast)) {
-        //Subir VIDEOJUEGO
-        
-        $sentence1 = $db->prepare("INSERT INTO files (TITLE, TYPE, EXT_IMG_ROUTE, USER_ID) VALUES (?, ?, ?, ?)");
-        $sentence1->bindParam(1, $title);
-        $sentence1->bindParam(2, $type);
-        $sentence1->bindParam(3, $ext_img_route);
-        $sentence1->bindParam(4, $userid);
-        $sentence1->execute();
-
-        $file_id = intval($db->lastInsertId());
-
-        $episode_titles = $_POST['episode_title'];
-        $episode_int_file_routes = $_FILES['episode_int_file_route'];
-        $episode_dates = $_POST['episode_date'];
-
-        if(isset($_FILES['episode_int_file_route'])) {
-            $numArchivos = count($_FILES['episode_int_file_route']['name']);
-        
-            for($i = 0; $i < $numArchivos; $i++) {
-                $lastEpisodeId = getNextIdEpisodes($db);
-                $int_file = 'public/files/' . $lastEpisodeId . '.torrent';
-                                
-                $nombreArchivo = $_FILES['episode_int_file_route']['name'][$i];
-                $tipoArchivo = $_FILES['episode_int_file_route']['type'][$i];
-                $tmpNombre = $_FILES['episode_int_file_route']['tmp_name'][$i];
     
-                $sentence2 = $db->prepare("INSERT INTO episodes (ID, EPISODE, INT_FILE_ROUTE, DATE, FILE_ID) VALUES (?, ?, ?, ?, ?)");
-                $sentence2->bindParam(1, $lastEpisodeId);
-                $sentence2->bindParam(2, $episode_titles[$i]);
-                $sentence2->bindParam(3, $int_file);
-                $sentence2->bindParam(4, $episode_dates[$i]);
-                $sentence2->bindParam(5, $file_id);
-                $sentence2->execute();
+            header('Location: ../../dashboard.php');
+            break;
+        case 'PELICULA':
+            $sentence1 = $db->prepare("INSERT INTO files (TITLE, DESCRIPTION, TYPE, GENRE, FORMAT, DIRECTOR, CAST, EXT_IMG_ROUTE, USER_ID) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+            $sentence1->bindParam(1, $title);
+            $sentence1->bindParam(2, $description);
+            $sentence1->bindParam(3, $type);
+            $sentence1->bindParam(4, $genre);
+            $sentence1->bindParam(5, $format);
+            $sentence1->bindParam(6, $director);
+            $sentence1->bindParam(7, $cast);
+            $sentence1->bindParam(8, $ext_img_route);
+            $sentence1->bindParam(9, $userid);
+            $sentence1->execute();
     
-                $rutaDestino = "../../public/files/" . $lastEpisodeId . '.torrent';
-                move_uploaded_file($tmpNombre, $rutaDestino);
-                
-                echo "El archivo $nombreArchivo se subió correctamente.";
+            $file_id = intval($db->lastInsertId());
+    
+            $episode_titles = $_POST['episode_title'];
+            $episode_int_file_routes = $_FILES['episode_int_file_route'];
+            $episode_dates = $_POST['episode_date'];
+    
+            if(isset($_FILES['episode_int_file_route'])) {
+                $numArchivos = count($_FILES['episode_int_file_route']['name']);
+            
+                for($i = 0; $i < $numArchivos; $i++) {
+                    $lastEpisodeId = getNextIdEpisodes($db);
+                    $int_file = 'public/files/' . $lastEpisodeId . '.torrent';
+                                    
+                    $nombreArchivo = $_FILES['episode_int_file_route']['name'][$i];
+                    $tipoArchivo = $_FILES['episode_int_file_route']['type'][$i];
+                    $tmpNombre = $_FILES['episode_int_file_route']['tmp_name'][$i];
+        
+                    $sentence2 = $db->prepare("INSERT INTO episodes (ID, EPISODE, INT_FILE_ROUTE, DATE, FILE_ID) VALUES (?, ?, ?, ?, ?)");
+                    $sentence2->bindParam(1, $lastEpisodeId);
+                    $sentence2->bindParam(2, $episode_titles[$i]);
+                    $sentence2->bindParam(3, $int_file);
+                    $sentence2->bindParam(4, $episode_dates[$i]);
+                    $sentence2->bindParam(5, $file_id);
+                    $sentence2->execute();
+        
+                    $rutaDestino = "../../public/files/" . $lastEpisodeId . '.torrent';
+                    move_uploaded_file($tmpNombre, $rutaDestino);
+                    
+                    echo "El archivo $nombreArchivo se subió correctamente.";
+                }
             }
-        }
+    
+            header('Location: ../../dashboard.php');
+            break;
+        case 'DOCUMENTAL':
+            $sentence1 = $db->prepare("INSERT INTO files (TITLE, DESCRIPTION, TYPE, FORMAT, EXT_IMG_ROUTE, USER_ID) VALUES (?, ?, ?, ?, ?, ?)");
+            $sentence1->bindParam(1, $title);
+            $sentence1->bindParam(2, $description);
+            $sentence1->bindParam(3, $type);
+            $sentence1->bindParam(4, $format);
+            $sentence1->bindParam(5, $ext_img_route);
+            $sentence1->bindParam(6, $userid);
+            $sentence1->execute();
 
-        header('Location: ../../dashboard.php');
-        die();
-    }else {
-        //Subir PELICULA
+            $file_id = intval($db->lastInsertId());
 
-        $sentence1 = $db->prepare("INSERT INTO files (TITLE, DESCRIPTION, TYPE, GENRE, FORMAT, DIRECTOR, CAST, EXT_IMG_ROUTE, USER_ID) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
-        $sentence1->bindParam(1, $title);
-        $sentence1->bindParam(2, $description);
-        $sentence1->bindParam(3, $type);
-        $sentence1->bindParam(4, $genre);
-        $sentence1->bindParam(5, $format);
-        $sentence1->bindParam(6, $director);
-        $sentence1->bindParam(7, $cast);
-        $sentence1->bindParam(8, $ext_img_route);
-        $sentence1->bindParam(9, $userid);
-        $sentence1->execute();
-
-        $file_id = intval($db->lastInsertId());
-
-        $episode_titles = $_POST['episode_title'];
-        $episode_int_file_routes = $_FILES['episode_int_file_route'];
-        $episode_dates = $_POST['episode_date'];
-
-        if(isset($_FILES['episode_int_file_route'])) {
-            $numArchivos = count($_FILES['episode_int_file_route']['name']);
+            $episode_titles = $_POST['episode_title'];
+            $episode_int_file_routes = $_FILES['episode_int_file_route'];
+            $episode_dates = $_POST['episode_date'];
+            
+            if(isset($_FILES['episode_int_file_route'])) {
+                $numArchivos = count($_FILES['episode_int_file_route']['name']);
+            
+                for($i = 0; $i < $numArchivos; $i++) {
+                    $lastEpisodeId = getNextIdEpisodes($db);
+                    $int_file = 'public/files/' . $lastEpisodeId . '.torrent';
+                                    
+                    $nombreArchivo = $_FILES['episode_int_file_route']['name'][$i];
+                    $tipoArchivo = $_FILES['episode_int_file_route']['type'][$i];
+                    $tmpNombre = $_FILES['episode_int_file_route']['tmp_name'][$i];
         
-            for($i = 0; $i < $numArchivos; $i++) {
-                $lastEpisodeId = getNextIdEpisodes($db);
-                $int_file = 'public/files/' . $lastEpisodeId . '.torrent';
-                                
-                $nombreArchivo = $_FILES['episode_int_file_route']['name'][$i];
-                $tipoArchivo = $_FILES['episode_int_file_route']['type'][$i];
-                $tmpNombre = $_FILES['episode_int_file_route']['tmp_name'][$i];
-    
-                $sentence2 = $db->prepare("INSERT INTO episodes (ID, EPISODE, INT_FILE_ROUTE, DATE, FILE_ID) VALUES (?, ?, ?, ?, ?)");
-                $sentence2->bindParam(1, $lastEpisodeId);
-                $sentence2->bindParam(2, $episode_titles[$i]);
-                $sentence2->bindParam(3, $int_file);
-                $sentence2->bindParam(4, $episode_dates[$i]);
-                $sentence2->bindParam(5, $file_id);
-                $sentence2->execute();
-    
-                $rutaDestino = "../../public/files/" . $lastEpisodeId . '.torrent';
-                move_uploaded_file($tmpNombre, $rutaDestino);
-                
-                echo "El archivo $nombreArchivo se subió correctamente.";
+                    $sentence2 = $db->prepare("INSERT INTO episodes (ID, EPISODE, INT_FILE_ROUTE, DATE, FILE_ID) VALUES (?, ?, ?, ?, ?)");
+                    $sentence2->bindParam(1, $lastEpisodeId);
+                    $sentence2->bindParam(2, $episode_titles[$i]);
+                    $sentence2->bindParam(3, $int_file);
+                    $sentence2->bindParam(4, $episode_dates[$i]);
+                    $sentence2->bindParam(5, $file_id);
+                    $sentence2->execute();
+        
+                    $rutaDestino = "../../public/files/" . $lastEpisodeId . '.torrent';
+                    move_uploaded_file($tmpNombre, $rutaDestino);
+                    
+                    echo "El archivo $nombreArchivo se subió correctamente.";
+                }
             }
-        }
-
-        header('Location: ../../dashboard.php');
-        die();
+    
+            header('Location: ../../dashboard.php');
+            break;
+        case 'VIDEOJUEGO':
+            $sentence1 = $db->prepare("INSERT INTO files (TITLE, TYPE, EXT_IMG_ROUTE, USER_ID) VALUES (?, ?, ?, ?)");
+            $sentence1->bindParam(1, $title);
+            $sentence1->bindParam(2, $type);
+            $sentence1->bindParam(3, $ext_img_route);
+            $sentence1->bindParam(4, $userid);
+            $sentence1->execute();
+    
+            $file_id = intval($db->lastInsertId());
+    
+            $episode_titles = $_POST['episode_title'];
+            $episode_int_file_routes = $_FILES['episode_int_file_route'];
+            $episode_dates = $_POST['episode_date'];
+    
+            if(isset($_FILES['episode_int_file_route'])) {
+                $numArchivos = count($_FILES['episode_int_file_route']['name']);
+            
+                for($i = 0; $i < $numArchivos; $i++) {
+                    $lastEpisodeId = getNextIdEpisodes($db);
+                    $int_file = 'public/files/' . $lastEpisodeId . '.torrent';
+                                    
+                    $nombreArchivo = $_FILES['episode_int_file_route']['name'][$i];
+                    $tipoArchivo = $_FILES['episode_int_file_route']['type'][$i];
+                    $tmpNombre = $_FILES['episode_int_file_route']['tmp_name'][$i];
+        
+                    $sentence2 = $db->prepare("INSERT INTO episodes (ID, EPISODE, INT_FILE_ROUTE, DATE, FILE_ID) VALUES (?, ?, ?, ?, ?)");
+                    $sentence2->bindParam(1, $lastEpisodeId);
+                    $sentence2->bindParam(2, $episode_titles[$i]);
+                    $sentence2->bindParam(3, $int_file);
+                    $sentence2->bindParam(4, $episode_dates[$i]);
+                    $sentence2->bindParam(5, $file_id);
+                    $sentence2->execute();
+        
+                    $rutaDestino = "../../public/files/" . $lastEpisodeId . '.torrent';
+                    move_uploaded_file($tmpNombre, $rutaDestino);
+                    
+                    echo "El archivo $nombreArchivo se subió correctamente.";
+                }
+            }
+    
+            header('Location: ../../dashboard.php');
+            break;
+        
+        default:
+            break;
     }
 ?>
