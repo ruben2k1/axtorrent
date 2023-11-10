4f04a7063ffc29d9d097100a415db41c9e0e3790
Rubén <56169753+ruben2k1@users.noreply.github.com>
2023-11-10 21:06:19 +0100

    Update files

--- archivo.php
+++ archivo.php
@@ -20,13 +20,7 @@
     <link rel="shortcut icon" href="public/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-K9PYVMMV1W"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'G-K9PYVMMV1W');
-    </script>
+    <script src="public/js/analytics.js"></script>
 </head>
 <body>
     <?php require('app/views/partials/header.php'); ?>
