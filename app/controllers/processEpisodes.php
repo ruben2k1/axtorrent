12be4cc8c79b26eb003f1d0d3c1f048821a52088
Rubén <56169753+ruben2k1@users.noreply.github.com>
2023-11-20 02:24:59 +0100

    Update files

--- app/controllers/processEpisodes.php
+++ app/controllers/processEpisodes.php
@@ -1,3 +1,55 @@
 <?php
+    require_once('../database/db.php');
+    require_once('functions/getNextIdEpisodes.php');
+
+    $db = new Connection('localhost', 'axtorrent', 3307, 'root', 'EC5B09B113AC14D6FF0481665B469AA560CE662E7E87BF57C344FC4E03844B8C');
+
+    $file_id = $_POST['file_id'];
+
+    $episode_titles = $_POST['episode_title'];
+    $episode_int_file_routes = $_FILES['episode_int_file_route'];
+    $episode_magnet_file_routes = $_POST['episode_magnet_file_route'];
+    $numArchivos = count($_FILES['episode_int_file_route']['name']);
+
+    if(isset($_FILES['episode_int_file_route'])) {
+        $numArchivos = count($_FILES['episode_int_file_route']['name']);
     
+        for($i = 0; $i < $numArchivos; $i++) {
+            $lastEpisodeId = getNextIdEpisodes($db);
+            $int_file = 'public/files/' . $lastEpisodeId . '.torrent';
+                            
+            $nombreArchivo = $_FILES['episode_int_file_route']['name'][$i];
+            $tipoArchivo = $_FILES['episode_int_file_route']['type'][$i];
+            $tmpNombre = $_FILES['episode_int_file_route']['tmp_name'][$i];
+            
+            if (empty($episode_magnet_file_routes[$i])) {
+                $sentence2 = $db->prepare("INSERT INTO episodes (ID, EPISODE, INT_FILE_ROUTE, DATE, FILE_ID) VALUES (?, ?, ?, CURRENT_TIMESTAMP, ?)");
+                $sentence2->bindParam(1, $lastEpisodeId);
+                $sentence2->bindParam(2, $episode_titles[$i]);
+                $sentence2->bindParam(3, $int_file);
+                $sentence2->bindParam(4, $file_id);
+                $sentence2->execute();
+    
+                $rutaDestino = "../../public/files/" . $lastEpisodeId . '.torrent';
+                move_uploaded_file($tmpNombre, $rutaDestino);
+                
+                echo "El archivo $nombreArchivo se subió correctamente.";
+            }else {
+                $sentence2 = $db->prepare("INSERT INTO episodes (ID, EPISODE, MAGNET_FILE_ROUTE, INT_FILE_ROUTE, DATE, FILE_ID) VALUES (?, ?, ?, ?, CURRENT_TIMESTAMP, ?)");
+                $sentence2->bindParam(1, $lastEpisodeId);
+                $sentence2->bindParam(2, $episode_magnet_file_routes[$i]);
+                $sentence2->bindParam(3, $episode_titles[$i]);
+                $sentence2->bindParam(4, $int_file);
+                $sentence2->bindParam(5, $file_id);
+                $sentence2->execute();
+    
+                $rutaDestino = "../../public/files/" . $lastEpisodeId . '.torrent';
+                move_uploaded_file($tmpNombre, $rutaDestino);
+                
+                echo "El archivo $nombreArchivo se subió correctamente.";
+            }
+        }
+    }
+
+    header('Location: ../../dashboard.php');
 ?>
--- dashboard.php
+++ dashboard.php
@@ -23,57 +23,57 @@
             fileForm.enctype = 'multipart/form-data';
 
             fileForm.innerHTML = `
-                <label for="title">Title</label>
-                <input type="text" name="title" id=""><br>
-                <label for="description">Description</label><br>
-                <textarea name="description" id="" cols="30" rows="10"></textarea><br>
-                <label for="type">Type</label>
-                <select name="type" id="">
-                    <option value="SERIE">SERIE</option>
-                    <option value="PELICULA">PELICULA</option>
-                    <option value="MUSICA">MUSICA</option>
-                    <option value="VIDEOJUEGO">VIDEOJUEGO</option>
-                    <option value="DOCUMENTAL">DOCUMENTAL</option>
-                    <option value="LIBRO">LIBRO</option>
-                </select><br>
-                <label for="genre">Genre</label>
-                <input type="text" name="genre" id=""><br>
-                <label for="format">Format</label>
-                <select name="format" id="">
-                    <option value="HDRip">HDRip</option>
-                    <option value="HDTV">HDTV</option>
-                    <option value="HDTV-720p">HDTV-720p</option>
-                    <option value="SAT-Rip">SAT-Rip</option>
-                    <option value="DVDRip">DVDRip</option>
-                    <option value="MicroHD-720p">MicroHD-720p</option>
-                    <option value="MicroHD-1080p">MicroHD-1080p</option>
-                    <option value="BluRay-720p">BluRay-720p</option>
-                    <option value="BluRay-1080p">BluRay-1080p</option>
-                    <option value="BDremux-1080p">BDremux-1080p</option>
-                    <option value="4K">4K</option>
-                </select><br>
-                <label for="release_date">Release date</label>
-                <input type="number" name="release_date" id=""><br>
-                <label for="director">Director</label>
-                <input type="text" name="director" id=""><br>
-                <label for="cast">Cast</label>
-                <input type="text" name="cast" id=""><br>
-                <label for="ext_img_route">External image route</label>
-                <input type="text" name="ext_img_route" id=""><br>
+            <label for="title">Title</label>
+            <input type="text" name="title" id=""><br>
+            <label for="description">Description</label><br>
+            <textarea name="description" id="" cols="30" rows="10"></textarea><br>
+            <label for="type">Type</label>
+            <select name="type" id="">
+                <option value="SERIE">SERIE</option>
+                <option value="PELICULA">PELICULA</option>
+                <option value="MUSICA">MUSICA</option>
+                <option value="VIDEOJUEGO">VIDEOJUEGO</option>
+                <option value="DOCUMENTAL">DOCUMENTAL</option>
+                <option value="LIBRO">LIBRO</option>
+            </select><br>
+            <label for="genre">Genre</label>
+            <input type="text" name="genre" id=""><br>
+            <label for="format">Format</label>
+            <select name="format" id="">
+                <option value="HDRip">HDRip</option>
+                <option value="HDTV">HDTV</option>
+                <option value="HDTV-720p">HDTV-720p</option>
+                <option value="SAT-Rip">SAT-Rip</option>
+                <option value="DVDRip">DVDRip</option>
+                <option value="MicroHD-720p">MicroHD-720p</option>
+                <option value="MicroHD-1080p">MicroHD-1080p</option>
+                <option value="BluRay-720p">BluRay-720p</option>
+                <option value="BluRay-1080p">BluRay-1080p</option>
+                <option value="BDremux-1080p">BDremux-1080p</option>
+                <option value="4K">4K</option>
+            </select><br>
+            <label for="release_date">Release date</label>
+            <input type="number" name="release_date" id=""><br>
+            <label for="director">Director</label>
+            <input type="text" name="director" id=""><br>
+            <label for="cast">Cast</label>
+            <input type="text" name="cast" id=""><br>
+            <label for="ext_img_route">External image route</label>
+            <input type="text" name="ext_img_route" id=""><br>
 
-                <h2>Episode 1</h2>
-                <label for="episode_title">Title</label>
-                <input type="text" name="episode_title[]" id=""><br>
-                <label for="episode_int_file_route">Internal file route</label>
-                <input type="file" name="episode_int_file_route[]" id=""><br>
-                <label for="episode_date">Date</label>
-                <input type="date" name="episode_date[]" id=""><br>
+            <h2>Episode 1</h2>
+            <label for="episode_title">Title</label>
+            <input type="text" name="episode_title[]" id=""><br>
+            <label for="episode_int_file_route">Internal file route</label>
+            <input type="file" name="episode_int_file_route[]" id=""><br>
+            <label for="episode_date">Date</label>
+            <input type="date" name="episode_date[]" id=""><br>
 
-                <div id="episodes"></div>
+            <div id="episodes"></div>
 
-                <button type="button" id="addEpisode">Add Episode</button><br><br>
+            <button type="button" id="addEpisode">Add Episode</button><br><br>
 
-                <input type="submit" value="Upload">`;
+            <input type="submit" value="Upload">`;
 
             document.body.appendChild(fileForm);
 
@@ -84,20 +84,61 @@
 
                 let episodeDiv = document.createElement('div');
                 episodeDiv.innerHTML = `
-                    <h2>Episode ${episodeCount}</h2>
-                    <label for="episode_title">Title</label>
-                    <input type="text" name="episode_title[]" id=""><br>
-                    <label for="episode_int_file_route">Internal file route</label>
-                    <input type="file" name="episode_int_file_route[]" id=""><br>
-                    <label for="episode_date">Date</label>
-                    <input type="date" name="episode_date[]" id=""><br>`;
-                    
+                <h2>Episode ${episodeCount}</h2>
+                <label for="episode_title">Title</label>
+                <input type="text" name="episode_title[]" id=""><br>
+                <label for="episode_int_file_route">Internal file route</label>
+                <input type="file" name="episode_int_file_route[]" id=""><br>
+                <label for="episode_date">Date</label>
+                <input type="date" name="episode_date[]" id=""><br>`;
+                
                 document.getElementById('episodes').appendChild(episodeDiv);
             });
         });
 
         document.getElementById('add').addEventListener('click', () => {
+            let fileForm = document.createElement('form');
+            fileForm.id = 'fileForm';
+            fileForm.action = 'app/controllers/processEpisodes.php';
+            fileForm.method = 'post';
+            fileForm.enctype = 'multipart/form-data';
+
+            fileForm.innerHTML = `
+            <label for="file_id">FILE ID</label>
+            <input type="number" name="file_id" id="">
+            <h2>Episode 1</h2>
+            <label for="episode_title">Title</label>
+            <input type="text" name="episode_title[]" id=""><br>
+            <label for="episode_magnet_file_route">MAGNET FILE ROUTE</label>
+            <input type="text" name="episode_magnet_file_route[]" id=""><br>
+            <label for="episode_int_file_route">Internal file route</label>
+            <input type="file" name="episode_int_file_route[]" id=""><br>
+
+            <div id="episodes"></div>
+
+            <button type="button" id="addEpisode">Add Episode</button><br><br>
+
+            <input type="submit" value="Upload">`;
+
+            document.body.appendChild(fileForm);
+
+            let episodeCount = 1;
             
+            document.getElementById('addEpisode').addEventListener('click', function () {
+                episodeCount++;
+
+                let episodeDiv = document.createElement('div');
+                episodeDiv.innerHTML = `
+                <h2>Episode ${episodeCount}</h2>
+                <label for="episode_title">Title</label>
+                <input type="text" name="episode_title[]" id=""><br>
+                <label for="episode_magnet_file_route">MAGNET FILE ROUTE</label>
+                <input type="text" name="episode_magnet_file_route[]" id=""><br>
+                <label for="episode_int_file_route">Internal file route</label>
+                <input type="file" name="episode_int_file_route[]" id=""><br>`;
+                
+                document.getElementById('episodes').appendChild(episodeDiv);
+            });
         })
     </script>
 </body>
