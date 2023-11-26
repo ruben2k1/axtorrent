1f806cc3cc4686c5b47f70d4795159eea30a6c21
Rubén <56169753+ruben2k1@users.noreply.github.com>
2023-11-26 01:54:33 +0100

    Update files

--- app/controllers/novedadesController.php
+++ app/controllers/novedadesController.php
@@ -3,7 +3,7 @@
 
     $db = new Connection('localhost', 'axtorrent', 3307, 'root', 'EC5B09B113AC14D6FF0481665B469AA560CE662E7E87BF57C344FC4E03844B8C');
 
-    $sentence1 = $db->prepare("SELECT * FROM files WHERE ID >= (SELECT MAX(ID) FROM files) - 19");
+    $sentence1 = $db->prepare("SELECT * FROM files GROUP BY UPLOAD_DATE DESC LIMIT 19");
     $sentence1->execute();
     $results1 = $sentence1->fetchAll();
 ?>
