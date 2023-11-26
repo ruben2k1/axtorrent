9819483940f200bfe8c461ae5aafaed8ee00fada
Rubén <56169753+ruben2k1@users.noreply.github.com>
2023-11-26 20:06:19 +0100

    Update files

--- app/views/partials/header.php
+++ app/views/partials/header.php
@@ -3,8 +3,8 @@
         <ul>
             <a href="/"><img src="/public/logo.svg" alt="Logotipo AXTorrent" class="logo"></a>
             <li><a href="/"><i class="bi bi-house-fill"></i> INICIO</a></li>
-            <li><a href="novedades.php"><i class="bi bi-brightness-high-fill"></i> NOVEDADES</a></li>
-            <li><a href="contacto.php"><i class="bi bi-cursor-fill"></i> CONTACTO</a></li>
+            <li><a href="/novedades"><i class="bi bi-brightness-high-fill"></i> NOVEDADES</a></li>
+            <li><a href="/contacto"><i class="bi bi-cursor-fill"></i> CONTACTO</a></li>
             <li>
                 <form action="buscar.php" method="get" class="nav-form">
                     <input type="text" name="titulo" autocomplete="off" placeholder="Buscar archivo...">
