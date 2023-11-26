c1b108ba7f6c0830604f0264591286dabcd5c64f
Rubén <56169753+ruben2k1@users.noreply.github.com>
2023-11-26 21:51:39 +0100

    Update files

--- contacto.php
+++ contacto.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contacto - AXTorrent</title>
-    <link rel="canonical" href="https://axtorrent.com/contacto.php">
-    <link rel="stylesheet" href="public/css/reset.css">
-    <link rel="stylesheet" href="public/css/index.css">
-    <link rel="shortcut icon" href="public/favicon.ico" type="image/x-icon">
+    <link rel="canonical" href="https://axtorrent.com/contacto">
+    <link rel="stylesheet" href="/public/css/reset.css">
+    <link rel="stylesheet" href="/public/css/index.css">
+    <link rel="shortcut icon" href="/public/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-K9PYVMMV1W"></script>
-    <script src="public/js/analytics.js"></script>
+    <script src="/public/js/analytics.js"></script>
 </head>
 <body>
     <?php require_once('app/views/partials/header.php'); ?>
