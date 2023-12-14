342e3850fd9a2dd10709b53a9fbcfd4f9e6f8628
Rubén <56169753+ruben2k1@users.noreply.github.com>
2023-12-14 02:35:13 +0100

    Update files

--- dashboard.php
+++ dashboard.php
@@ -16,6 +16,126 @@
 
     <script>
         document.getElementById('create').addEventListener('click', function () {
+            function generateSerieFields(){
+                return `
+                <div id="serieFields">
+                    <label for="format">Formato</label>
+                    <select name="format" id="">
+                        <option value="HDRip">HDRip</option>
+                        <option value="HDTV" selected>HDTV</option>
+                        <option value="HDTV-720p">HDTV-720p</option>
+                        <option value="SAT-Rip">SAT-Rip</option>
+                        <option value="DVDRip">DVDRip</option>
+                        <option value="MicroHD-720p">MicroHD-720p</option>
+                        <option value="MicroHD-1080p">MicroHD-1080p</option>
+                        <option value="BluRay-720p">BluRay-720p</option>
+                        <option value="BluRay-1080p">BluRay-1080p</option>
+                        <option value="BDremux-1080p">BDremux-1080p</option>
+                        <option value="4K">4K</option>
+                        <option value="PC">PC</option>
+                    </select><br>
+                    <label for="release_date">Año de estreno</label>
+                    <input type="number" name="release_date" id=""><br>
+                    <label for="ext_img_route">Ruta imagen externa</label>
+                    <input type="text" name="ext_img_route" id=""><br>
+                </div>`
+            }
+
+            function generatePeliculaFields(){
+                return `
+                <div id="peliculaFields">
+                    <label for="genre">Género</label>
+                    <input type="text" name="genre" id=""><br>
+                    <label for="format">Formato</label>
+                    <select name="format" id="">
+                        <option value="HDRip">HDRip</option>
+                        <option value="HDTV" selected>HDTV</option>
+                        <option value="HDTV-720p">HDTV-720p</option>
+                        <option value="SAT-Rip">SAT-Rip</option>
+                        <option value="DVDRip">DVDRip</option>
+                        <option value="MicroHD-720p">MicroHD-720p</option>
+                        <option value="MicroHD-1080p">MicroHD-1080p</option>
+                        <option value="BluRay-720p">BluRay-720p</option>
+                        <option value="BluRay-1080p">BluRay-1080p</option>
+                        <option value="BDremux-1080p">BDremux-1080p</option>
+                        <option value="4K">4K</option>
+                        <option value="PC">PC</option>
+                    </select><br>
+                    <label for="release_date">Año de estreno</label>
+                    <input type="number" name="release_date" id=""><br>
+                    <label for="director">Director</label>
+                    <input type="text" name="director" id=""><br>
+                    <label for="cast">Reparto</label>
+                    <input type="text" name="cast" id=""><br>
+                    <label for="ext_img_route">Ruta imagen externa</label>
+                    <input type="text" name="ext_img_route" id=""><br>
+                </div>`
+            }
+
+            function generateVideojuegoFields(){
+                return `
+                <div id="videojuegoFields">
+                    <label for="genre">Género</label>
+                    <input type="text" name="genre" id=""><br>
+                    <label for="format">Formato</label>
+                    <select name="format" id="">
+                        <option value="HDRip">HDRip</option>
+                        <option value="HDTV" selected>HDTV</option>
+                        <option value="HDTV-720p">HDTV-720p</option>
+                        <option value="SAT-Rip">SAT-Rip</option>
+                        <option value="DVDRip">DVDRip</option>
+                        <option value="MicroHD-720p">MicroHD-720p</option>
+                        <option value="MicroHD-1080p">MicroHD-1080p</option>
+                        <option value="BluRay-720p">BluRay-720p</option>
+                        <option value="BluRay-1080p">BluRay-1080p</option>
+                        <option value="BDremux-1080p">BDremux-1080p</option>
+                        <option value="4K">4K</option>
+                        <option value="PC">PC</option>
+                    </select><br>
+                    <label for="release_date">Año de estreno</label>
+                    <input type="number" name="release_date" id=""><br>
+                    <label for="ext_img_route">Ruta imagen externa</label>
+                    <input type="text" name="ext_img_route" id=""><br>
+                </div>`
+            }
+
+            function generateDocumentalFields(){
+                return `
+                <div id="documentalFields">
+                    <label for="format">Formato</label>
+                    <select name="format" id="">
+                        <option value="HDRip">HDRip</option>
+                        <option value="HDTV" selected>HDTV</option>
+                        <option value="HDTV-720p">HDTV-720p</option>
+                        <option value="SAT-Rip">SAT-Rip</option>
+                        <option value="DVDRip">DVDRip</option>
+                        <option value="MicroHD-720p">MicroHD-720p</option>
+                        <option value="MicroHD-1080p">MicroHD-1080p</option>
+                        <option value="BluRay-720p">BluRay-720p</option>
+                        <option value="BluRay-1080p">BluRay-1080p</option>
+                        <option value="BDremux-1080p">BDremux-1080p</option>
+                        <option value="4K">4K</option>
+                        <option value="PC">PC</option>
+                    </select><br>
+                    <label for="release_date">Año de estreno</label>
+                    <input type="number" name="release_date" id=""><br>
+                    <label for="ext_img_route">Ruta imagen externa</label>
+                    <input type="text" name="ext_img_route" id=""><br>
+                </div>`
+            }
+
+            function generateMusicaFields(){
+                return `
+                <div id="musicaFields">
+                    <label for="genre">Género</label>
+                    <input type="text" name="genre" id=""><br>
+                    <label for="release_date">Año de estreno</label>
+                    <input type="number" name="release_date" id=""><br>
+                    <label for="ext_img_route">Ruta imagen externa</label>
+                    <input type="text" name="ext_img_route" id=""><br>
+                </div>`
+            }
+            
             let fileForm = document.createElement('form');
             fileForm.id = 'fileForm';
             fileForm.action = 'app/controllers/processFile.php';
@@ -23,44 +143,22 @@
             fileForm.enctype = 'multipart/form-data';
 
             fileForm.innerHTML = `
-            <label for="title">Title</label>
-            <input type="text" name="title" id=""><br>
-            <label for="description">Description</label><br>
-            <textarea name="description" id="" cols="30" rows="10"></textarea><br>
-            <label for="type">Type</label>
-            <select name="type" id="">
-                <option value="SERIE">SERIE</option>
-                <option value="PELICULA">PELICULA</option>
-                <option value="MUSICA">MUSICA</option>
-                <option value="VIDEOJUEGO">VIDEOJUEGO</option>
-                <option value="DOCUMENTAL">DOCUMENTAL</option>
-                <option value="LIBRO">LIBRO</option>
-            </select><br>
-            <label for="genre">Genre</label>
-            <input type="text" name="genre" id=""><br>
-            <label for="format">Format</label>
-            <select name="format" id="">
-                <option value="HDRip">HDRip</option>
-                <option value="HDTV" selected>HDTV</option>
-                <option value="HDTV-720p">HDTV-720p</option>
-                <option value="SAT-Rip">SAT-Rip</option>
-                <option value="DVDRip">DVDRip</option>
-                <option value="MicroHD-720p">MicroHD-720p</option>
-                <option value="MicroHD-1080p">MicroHD-1080p</option>
-                <option value="BluRay-720p">BluRay-720p</option>
-                <option value="BluRay-1080p">BluRay-1080p</option>
-                <option value="BDremux-1080p">BDremux-1080p</option>
-                <option value="4K">4K</option>
-                <option value="PC">PC</option>
-            </select><br>
-            <label for="release_date">Release date</label>
-            <input type="number" name="release_date" id=""><br>
-            <label for="director">Director</label>
-            <input type="text" name="director" id=""><br>
-            <label for="cast">Cast</label>
-            <input type="text" name="cast" id=""><br>
-            <label for="ext_img_route">External image route</label>
-            <input type="text" name="ext_img_route" id=""><br>
+            <div id="mainInfo">
+                <label for="title">Title</label>
+                <input type="text" name="title" id=""><br>
+                <label for="description">Description</label><br>
+                <textarea name="description" id="" cols="30" rows="10"></textarea><br>
+                <label for="type">Type</label>
+                <select name="type" id="typeSelect">
+                    <option value=""></option>
+                    <option value="SERIE">SERIE</option>
+                    <option value="PELICULA">PELICULA</option>
+                    <option value="MUSICA">MUSICA</option>
+                    <option value="VIDEOJUEGO">VIDEOJUEGO</option>
+                    <option value="DOCUMENTAL">DOCUMENTAL</option>
+                    <option value="LIBRO">LIBRO</option>
+                </select><br>
+            </div>
 
             <h2>Episode 1</h2>
             <label for="episode_title">Title</label>
